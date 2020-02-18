481dc9669c6fc777155a34b78300e20fc2695e5a
Namit Doshi <namitdoshi90@gmail.com>
2020-02-18 16:23:57 +0530

    created search form and sort button

--- view.php
+++ view.php
@@ -1,5 +1,3 @@
-
-
 <head>
   <title>View Details</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
@@ -7,26 +5,26 @@
 
 <body>
 
-<div class="container-fluid">
-<table>
-    <thead>
-      <tr>
-        <th>Id</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Mobile Number</th>
-        <th>Age</th>
-        <th>Hobby</th>
-        <th>Gender</th>
-        <th>Address</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
+  <div class="container-fluid">
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>First Name <a href="" class="waves-effect waves-light btn" name="sort">Sort</a></th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Mobile Number</th>
+          <th>Age</th>
+          <th>Hobby</th>
+          <th>Gender</th>
+          <th>Address</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
 
-    <tbody>
-  <?php
+      <tbody>
+        <?php
     include './connection.php';
     // print $_GET['id'];
     if (isset($_GET['id'])) {
@@ -46,29 +44,42 @@
       while($row=$result->fetch_assoc()) {
  
   ?>
-      <tr>
-        <td><?php echo $row['id'] ?></td>
-        <td><?php echo $row['fname'] ?></td>
-        <td><?php echo $row['lname'] ?></td>
-        <td><?php echo $row['email'] ?></td>
-        <td><?php echo $row['mobile'] ?></td>
-        <td><?php echo $row['age'] ?></td>
-        <td><?php echo $row['hobby'] ?></td>
-        <td><?php echo $row['gender'] ?></td>
-        <td><?php echo $row['address'] ?></td>
-        <td><a class="waves-effect waves-light btn" href="./edit.php?id=<?php echo $row['id']?>">Edit</a></td>
-        <td><a class="waves-effect waves-light btn" href="?id=<?php echo $row['id']; ?>" name="delete">Delete</a></td>
-      </tr>
-      <?php
+        <tr>
+          <td><?php echo $row['id'] ?></td>
+          <td><?php echo $row['fname'] ?></td>
+          <td><?php echo $row['lname'] ?></td>
+          <td><?php echo $row['email'] ?></td>
+          <td><?php echo $row['mobile'] ?></td>
+          <td><?php echo $row['age'] ?></td>
+          <td><?php echo $row['hobby'] ?></td>
+          <td><?php echo $row['gender'] ?></td>
+          <td><?php echo $row['address'] ?></td>
+          <td><a class="waves-effect waves-light btn" href="./edit.php?id=<?php echo $row['id']?>">Edit</a></td>
+          <td><a class="waves-effect waves-light btn" href="?id=<?php echo $row['id']; ?>" name="delete">Delete</a></td>
+        </tr>
+        <?php
       }
     }
   else {
     print 'fail';
   }
 ?>
-    </tbody>
-  </table>
-</div>
+      </tbody>
+    </table>
+    <div>
+      <form method="POST" class="col s12 center">
+        <div class="row center">
+          <div class="input-field col-3">
+            <input id="search-box" type="text" class="validate" name="search-box" style="width: 20%">
+          </div>
+          <div class="input-field col-3">
+            <!-- <input type="submit" value="Search" class="waves-effect waves-light btn"> -->
+            <a href="" class="waves-effect waves-light btn">Search</a>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
