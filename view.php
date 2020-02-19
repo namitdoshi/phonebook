930079471e5ac36c1a12e2562c06a9c21d6b0a81
Namit Doshi <namitdoshi90@gmail.com>
2020-02-19 10:17:34 +0530

    added jquery scripts in project

--- view.php
+++ view.php
@@ -125,5 +125,10 @@
     </div>
   </div>
   <!-- Compiled and minified JavaScript -->
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script src="./main.js"></script>
 </body>
