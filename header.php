be021ca4820d335238eee4caf9fc2693883d9c1b
Namit Doshi <namitdoshi90@gmail.com>
2020-02-24 12:05:55 +0530

    created a common header-nav

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="./index.php" class="brand-logo">Phone Book</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="./add-contact.php">Add Contact</a></li>
+        <li><a href="./view.php">View Contacts</a></li>
+        <li><a href="./add-group.php">Add Group</a></li>
+      </ul>
+    </div>
+  </nav>
