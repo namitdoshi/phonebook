7463db91182647c3b992025007f1c6556d2eca6c
Namit Doshi <namitdoshi90@gmail.com>
2020-02-24 17:18:50 +0530

    form method added and fields made not required

--- signup.php
+++ signup.php
@@ -1,5 +1,6 @@
 <?php 
   if (isset($_POST['submit'])) {
+    echo 'a';
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $email = $_POST['email'];
@@ -7,6 +8,9 @@
     $password = $_POST['password'];
     $repassword = $_POST['repassword'];
     
+    if ($password != $repassword) {
+      echo 'password do not match!';
+    }
   }
 ?>
 
@@ -24,40 +28,40 @@
 
 <body>
   <div class="container">
-    <form class="s 12 ">
+    <form class="s 12" method="POST">
       <div class="row">
         <div class="input-field col s6">
-          <input id="first_name" type="text" class="validate" required>
-          <label for="first_name">First Name</label>
+          <input id="fname" type="text" class="validate">
+          <label for="fname">First Name</label>
         </div>
         <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate" required>
-          <label for="last_name">Last Name</label>
+          <input id="lname" type="text" class="validate" >
+          <label for="lname">Last Name</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          <input id="email" type="text" class="validate" required>
+          <input id="email" type="text" class="validate" >
           <label for="email">Email</label>
         </div>
         <div class="input-field col s6">
-          <input id="mobile" type="text" class="validate" required>
+          <input id="mobile" type="text" class="validate" >
           <label for="mobile">Mobile</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          <input id="password" type="text" class="validate" required>
+          <input id="password" type="text" class="validate" >
           <label for="password">Password</label>
         </div>
         <div class="input-field col s6">
-          <input id="repassword" type="text" class="validate" required>
+          <input id="repassword" type="text" class="validate" >
           <label for="repassword">Re enter password</label>
         </div>
       </div>
       <div class="row">
         <div class="center">
-          <input type="submit" value="Submit" class="input-field btn">
+          <input type="submit" value="Submit" class="input-field btn" name="submit">
         </div>
       </div>
     </form>
