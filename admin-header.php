739d7d42ba35ea220f89a575881739c01757ef12
Namit Doshi <namitdoshi90@gmail.com>
2020-02-26 14:45:02 +0530

    created and added navbar for admin

---
+++ admin-header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $title; ?></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="./admin.php" class="brand-logo">Phone Book</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="./logout.php">Logout</a></li>
+      </ul>
+    </div>
+  </nav>
--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
-<?
+<?php
   session_start();
   if (isset($_SESSION['id'])) {
-    
+    include ('./admin-header.php');
   } else {
     header('location: ./login.php');
   }
