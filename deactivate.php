bac8f870383e613be73f809880776984734e4644
Namit Doshi <namitdoshi90@gmail.com>
2020-02-27 16:10:00 +0530

    session destroy and user logged out upon account deactivation

--- deactivate.php
+++ deactivate.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
+// $title = 'Account Deactivated';
+// include './header.php';
 include './connection.php';
 $user_id = $_SESSION['id'];
 if (isset($_SESSION['id'])) {
   $deactivateQuery = "UPDATE registeration r, user_details u, group_contacts_list g1, group_details g2 SET r.status = 'deactivated', u.status = 'deactivated', g1.status = 'deactivated', g2.status = 'deactivated' WHERE (r.user_id = '$user_id' AND u.user_id = '$user_id' AND g1.user_id = '$user_id' AND g2.user_id = '$user_id')";
   $deactivate = $con -> query($deactivateQuery);
   if ($deactivate) {
-    header('location: ./index.php');
+    session_destroy();
+    header('location: ./login.php');
+    // UPDATE registeration r, user_details u, group_contacts_list g1, group_details g2 SET r.status = 'active', u.status = 'active', g1.status = 'active', g2.status = 'active' WHERE (r.user_id = 5 AND u.user_id = 5 AND g1.user_id = 5 AND g2.user_id = 5)
   }
 } else {
   header('loccation: ./login.php');
