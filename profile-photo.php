737b98e989bf27dc50b71245ae2ef6c48d4938be
Namit Doshi <namitdoshi90@gmail.com>
2020-02-29 16:52:20 +0530

    session added

--- profile-photo.php
+++ profile-photo.php
@@ -1,7 +1,12 @@
 <?php
-  $title = 'Add profile photo';
-  include './header.php';
-  include './connection.php';
+  session_start();
+  if (isset($_SESSION['id'])) {
+    $title = 'Add profile photo';
+    include './header.php';
+    include './connection.php';
 
+  } else {
+    header('./login.php');
+  }
   
 ?>
